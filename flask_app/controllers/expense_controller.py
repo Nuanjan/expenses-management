7fb2b73940e9c55b42c978ca3c6860e65f1675ef
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-20 14:24:50 -0400

    Delete category Model

--- flask_app/controllers/expense_controller.py
+++ flask_app/controllers/expense_controller.py
@@ -1,6 +1,7 @@
 from flask.helpers import flash
 from werkzeug.datastructures import RequestCacheControl
 from flask_app import app
+from flask_app.models.expense import Expense
 from flask_app.models.user import User
 from flask import redirect, render_template, session, request, url_for
 from flask_app.models.buget import Budget
@@ -15,3 +16,21 @@ def new_expense():
     }
     one_user = User.get_user_by_id(user_data)
     return render_template('add_expense.html', one_user=one_user)
+
+
+@app.route('/add_expense', methods=['POST'])
+def add_expense():
+    if not 'user_id' in session:
+        return render_template('forbidden.html')
+    print("expense form data", request.form)
+    if not Expense.validate_expense(request.form):
+        return redirect('/new/expense')
+    data = {
+        "expense": request.form['expense'],
+        "date": request.form['date'],
+        "amount": request.form['amount'],
+        "category": request.form['category'],
+        "user_id": session['user_id']
+    }
+    Expense.add_expense(data)
+    return redirect('/user_dashboard')
--- flask_app/models/expense.py
+++ flask_app/models/expense.py
@@ -10,7 +10,31 @@ class Expense:
         self.title = data['title']
         self.date = data['date']
         self.amount = data['amount']
+        self.category = data['category']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
-        self.category_id = data['category_id']
+
+    @staticmethod
+    def validate_expense(expensesFormData):
+        is_valid = True
+        if expensesFormData['expense'] == "" or expensesFormData['date'] == "" or expensesFormData['category'] == "none":
+            flash("All filed required")
+            is_valid = False
+        elif expensesFormData['amount'] == "":
+            flash("All filed required")
+        elif float(expensesFormData['amount']) < 1.00:
+            flash('Amount must be greater than 0')
+            is_valid = False
+        return is_valid
+
+    @classmethod
+    def add_expense(cls, data):
+        query = "INSERT INTO expenses (expense, date, amount, category, created_at, updated_at, user_id) VALUES(%(expense)s, %(date)s, %(amount)s, %(category)s,NOW(), NOW(),%(user_id)s)"
+        results = connectToMySQL(
+            'expenses_management_schema').query_db(query, data)
+        return results
+
+    @classmethod
+    def all_expenses_with_owner(cls,data):
+        
