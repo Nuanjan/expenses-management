45b18b5d430db76ccaf908ab36e5bae95eb25b52
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-20 00:49:28 -0400

    Create expense and category model

--- flask_app/models/category.py
+++ flask_app/models/category.py
@@ -2,15 +2,12 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_app.models import user
 from flask_app.models import buget
+from flask_app.models import expense
 
 
-class Expense:
+class Category:
     def __init__(self, data):
         self.id = data['id']
-        self.title = data['title']
-        self.date = data['date']
-        self.amount = data['amount']
+        self.category = data['category']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-        self.user_id = data['user_id']
-        self.category_id = data['category_id']
