e43e6730d23899f957ddec55adcbd8de643e5f9a
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-21 14:40:55 -0400

    styling the template.

--- flask_app/controllers/expense_controller.py
+++ flask_app/controllers/expense_controller.py
@@ -19,6 +19,7 @@ def new_expense():
 
 @app.route('/add_expense', methods=['POST'])
 def add_expense():
+    print("budget_id", session['budget_id'])
     if not 'user_id' in session:
         return render_template('forbidden.html')
     print("expense form data", request.form)
@@ -26,18 +27,22 @@ def add_expense():
         return redirect('/new/expense')
     if not 'budget_id' in session:
         session['budget_id'] = 0
-    print("budget id in expenses controller ", session['budget_id'])
-    data = {
-        "expense": request.form['expense'],
-        "date": request.form['date'],
-        "amount": request.form['amount'],
-        "category": request.form['category'],
-        "user_id": session['user_id'],
-        "budget_id": session['budget_id']
 
-    }
-    Expense.add_expense(data)
-    return redirect('/user_dashboard')
+    if session['budget_id'] == 0:
+        flash("You must add the budget before add the expense")
+        return redirect('/user_dashboard')
+    else:
+        data = {
+            "expense": request.form['expense'],
+            "date": request.form['date'],
+            "amount": request.form['amount'],
+            "category": request.form['category'],
+            "user_id": session['user_id'],
+            "budget_id": session['budget_id']
+
+        }
+        Expense.add_expense(data)
+        return redirect('/user_dashboard')
 
 
 @app.route('/edit_expense/<int:expense_id>')
